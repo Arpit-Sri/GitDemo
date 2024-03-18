e4c4dbb7ceb9b2af6b52531a5c0fc2f236e1111a
Arpit-Sri <arpitsri04@gmail.com>
2024-03-18 12:47:54 -0700

    coding from middle

--- src/test/java/com/tutorialsninja/qa/testcases/Demo.java
+++ src/test/java/com/tutorialsninja/qa/testcases/Demo.java
@@ -5,7 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("have written code from scratch by Indian");
-	
+		System.out.println("now I have cloning this code from github and starting working fresh from here by American");
+		System.out.println("some chages after cloning the code ");
+		System.out.println("new code");
 		
 
 	}
