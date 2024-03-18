1c768a9a33aca01ab0876f36686324a3a78b777a
Arpit-Sri <arpitsri04@gmail.com>
2024-03-18 12:20:46 -0700

    Some Small chages

--- src/test/java/com/tutorialsninja/qa/testcases/Demo.java
+++ src/test/java/com/tutorialsninja/qa/testcases/Demo.java
@@ -1,20 +1,11 @@
 package com.tutorialsninja.qa.testcases;
 
-import java.util.Scanner;
-
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter your age :");
-		
-		int x= sc.nextInt();
-		int y=sc.nextInt();
-		
-		int z=x+y;
-		System.out.println(z);
+		System.out.println("have written code from scratch by Indian");
+	
 		
 
 	}
